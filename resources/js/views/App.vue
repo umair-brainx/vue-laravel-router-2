77698841d40aaaab8aa050e9e392e773b7361fe0
Umair <umair.ashraf@brainxtech.com>
2020-01-02 15:58:34 +0500

    first commit

---
+++ resources/js/views/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <div>
+                <router-link tag="li" to="/">Home</router-link>
+                <router-link tag="li" to="/about">About</router-link>
+
+                <router-view></router-view>
+        </div>
+        <div>Footer</div>
+    </div>
+
+
+</template>
+
+<script>
+    export default {};
+</script>
